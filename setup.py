fc82da1c3c616c3bdf3509e5e77e596df1c7d9f4
Rodrigo Ancavil <rancavil@gmail.com>
2015-04-06 23:16:42 -0300

    deploy django 1.8

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='django3',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
+      description='OpenShift Django 1.8 App',
+      author='Rodrigo Ancavil',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django==1.8'],
      )
