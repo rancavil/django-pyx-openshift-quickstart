9f173c876a8d1d83c57a6abd43afd29579200f4d
Rodrigo Ancavil <rancavil@gmail.com>
2015-04-20 22:29:04 -0300

    actived the url application root

--- openshift/openshift/urls.py
+++ openshift/openshift/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'openshift.views.home', name='home'),
+    url(r'^$', 'openshift.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
